80ac41f2f7a08aeb3883458fc7598e9a5cbdd3ef
Istiak-ovi <67536150+Istiak-ovi@users.noreply.github.com>
2020-11-10 19:01:51 +0600

    Done

--- Web_Tech_Lab_Final/Login.php
+++ Web_Tech_Lab_Final/Login.php
@@ -1,31 +1,41 @@
-<?php include_once "verify.php" ;?>
+<?php include "validate_login.php" ?>
 <html>
-	<head></head>
-	<body>
-		
-		<fieldset>  
-		* Fields are required
-			<form action="" method="post">
-				<table>
-					<tr>
-						<td>Username:</td>
-						<td><input type="text" value="<?php echo $uname?>" name="uname"></td>
-						<td><span style="color:red;">*<?php echo $err_uname;?></span>
-						</td>
-					</tr>
-					<tr>
-						<td>Password:</td>
-						<td><input type="password" value="<?php echo $pass?>" name="pass"></td>
-						<td><span style="color:red;">*<?php echo $err_pass;?></span>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2" align="right">
-							<input type="submit" name="login" value="login">
-						</td>
-					</tr>
-				</table>
-			</form>
-		</fieldset>
-	</body>
+<head>
+	<title>Login</title>
+</head>
+<body>
+    <form action="" method="post" style="width: 300px";>
+    	<fieldset style="background-color: lightgray;">
+    		<table align="center">
+    			<h1 align="center">Login</h1>
+    			<tr style="line-height: 50px;">
+    				<td>
+    					Username:
+    				</td>
+    			</tr>
+    			<tr style="line-height: 50px;">
+    				<td>
+    					<input type="text" name="uname">
+    				</td>
+    			</tr>
+    			<tr style="line-height: 50px;">
+    				<td>
+    					Password:
+    				</td>
+    			</tr>
+    			<tr style="line-height: 50px;">
+    				<td>
+    					<input type="Password" name="pass">
+    					<input type="submit" name="login" value="Login" style="background-color: green; color: white";>
+    				</td>
+    			</tr>
+    			<tr>
+    				<td>
+    					<a href="reg.php">Create a new account</a>
+    				</td>
+    			</tr>
+    		</table>
+    	</fieldset>
+    </form>
+</body>
 </html>
